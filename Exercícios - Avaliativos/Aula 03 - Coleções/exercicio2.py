03e232a6d4ed1e7eeeff9c3f62bda4932fbef7f8
G-Ilian <gbiel.boza@gmail.com>
2022-04-22 21:39:54 -0300

    Coleções

---
+++ Exercícios - Avaliativos/Aula 03 - Coleções/exercicio2.py
@@ -0,0 +1,8 @@
+#### Exercício 2 - Lojas ######
+loja1={'Xiaomi','Asus','Apple'}
+loja2={'Motorola','Huawei','Samsung'}
+
+print(f'''A 1ª loja tem {len(loja1)} dispositivo(s) disponíveis\n
+Os dispositivos são {loja1}\n''')
+print(f'''A 2ª loja tem {len(loja2)} dispositivo(s) disponíveis\n
+Os dispositivos são {loja2}''')
